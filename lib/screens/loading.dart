fa7fc533b1c4c552cb6fc5ece77b0caa03dd6d65
BG <ohnsbin@gmail.com>
2022-07-06 12:58:54 +0900

    third

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'result_screen.dart';
 
 const apiKey = '1bac530ca18c4cc3b688';
-const foodName = '후루룩국수';
+const foodName = '신라면';
 
 class Loading extends StatefulWidget {
   @override
@@ -25,19 +25,30 @@ class _LoadingState extends State<Loading> {
     http.Response response = await http.get(url);
     if(response.statusCode == 200){
       String jsonData = response.body;
-      var GROUP_NAME = jsonDecode(jsonData)['I2790']['row'][0]['GROUP_NAME'];
-      var DESC_KOR = jsonDecode(jsonData)['I2790']['row'][0]['DESC_KOR'];
-      var MAKER_NAME = jsonDecode(jsonData)['I2790']['row'][0]['MAKER_NAME'];
-      var SERVING_SIZE = jsonDecode(jsonData)['I2790']['row'][0]['SERVING_SIZE'];
-      var NUTR_CONT1 = jsonDecode(jsonData)['I2790']['row'][0]['NUTR_CONT1'];
-      var NUTR_CONT2 = jsonDecode(jsonData)['I2790']['row'][0]['NUTR_CONT2'];
-      var NUTR_CONT3 = jsonDecode(jsonData)['I2790']['row'][0]['NUTR_CONT3'];
-      var NUTR_CONT4 = jsonDecode(jsonData)['I2790']['row'][0]['NUTR_CONT4'];
-      var NUTR_CONT5 = jsonDecode(jsonData)['I2790']['row'][0]['NUTR_CONT5'];
-      var NUTR_CONT6 = jsonDecode(jsonData)['I2790']['row'][0]['NUTR_CONT6'];
-      var NUTR_CONT7 = jsonDecode(jsonData)['I2790']['row'][0]['NUTR_CONT7'];
-      var NUTR_CONT8 = jsonDecode(jsonData)['I2790']['row'][0]['NUTR_CONT8'];
-      var NUTR_CONT9 = jsonDecode(jsonData)['I2790']['row'][0]['NUTR_CONT9'];
+
+      int i = 0;
+      while (foodName != jsonDecode(jsonData)['I2790']['row'][i]['DESC_KOR']) {
+        i += 1;
+        if (i == 5) {
+          i = 0;
+          break;
+        }
+      }
+
+
+      var GROUP_NAME = jsonDecode(jsonData)['I2790']['row'][i]['GROUP_NAME'];
+      var DESC_KOR = jsonDecode(jsonData)['I2790']['row'][i]['DESC_KOR'];
+      var MAKER_NAME = jsonDecode(jsonData)['I2790']['row'][i]['MAKER_NAME'];
+      var SERVING_SIZE = jsonDecode(jsonData)['I2790']['row'][i]['SERVING_SIZE'];
+      var NUTR_CONT1 = jsonDecode(jsonData)['I2790']['row'][i]['NUTR_CONT1'];
+      var NUTR_CONT2 = jsonDecode(jsonData)['I2790']['row'][i]['NUTR_CONT2'];
+      var NUTR_CONT3 = jsonDecode(jsonData)['I2790']['row'][i]['NUTR_CONT3'];
+      var NUTR_CONT4 = jsonDecode(jsonData)['I2790']['row'][i]['NUTR_CONT4'];
+      var NUTR_CONT5 = jsonDecode(jsonData)['I2790']['row'][i]['NUTR_CONT5'];
+      var NUTR_CONT6 = jsonDecode(jsonData)['I2790']['row'][i]['NUTR_CONT6'];
+      var NUTR_CONT7 = jsonDecode(jsonData)['I2790']['row'][i]['NUTR_CONT7'];
+      var NUTR_CONT8 = jsonDecode(jsonData)['I2790']['row'][i]['NUTR_CONT8'];
+      var NUTR_CONT9 = jsonDecode(jsonData)['I2790']['row'][i]['NUTR_CONT9'];
 
       Navigator.push(context, MaterialPageRoute(builder: (context){
         return ResultScreen(GROUP_NAME: GROUP_NAME, DESC_KOR: DESC_KOR,
